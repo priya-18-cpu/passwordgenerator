56c6b0b734b2cb77f215b117dbbe425c6a78df7e
priya-18-cpu <priyadhanapal086@gmail.com>
2025-07-24 13:31:27 +0530

    Add files via upload

---
+++ password generator.py
@@ -0,0 +1,16 @@
+import random
+import string
+
+def generate_password(length):
+    characters=string.ascii_letters+string.digits+string.punctuation
+    password=''.join(random.choice(characters)for _ in range(length))
+    return password
+
+def password_generator():
+    print("Password Generator")
+    length=int(input("Enter the desired password length:"))
+    password=generate_password(length)
+    
+    print(f"Generate password:{password}")
+    
+password_generator()
